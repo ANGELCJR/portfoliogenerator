77ab366f7bfd7b1719e08c7c255a9f49b7b8c449
Your Name <angeltcastillo11@gmail.com>
2022-07-19 20:42:38 -0500

    having issue

---
+++ app.js
@@ -0,0 +1,28 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const generatePage = (userName, githubName) => {
+  return `
+    Name: ${userName}
+    GitHub: ${githubName}
+  `;
+};
+console.log(generatePage(`name`, `github`));
+const profileDataArgs = process.argv.slice(2);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+
+printProfileData(profileDataArgs);
+
+  
+
