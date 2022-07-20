edadfc0de9dea3b089bd40991f1e82ee0d2b462c
Your Name <angeltcastillo11@gmail.com>
2022-07-20 12:33:17 -0500

    new code

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const generatePage = (userName, githubName) => {
-  return `
-    Name: ${userName}
-    GitHub: ${githubName}
-  `;
-};
-console.log(generatePage(name, github));
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
+console.log(profileDataArgs);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
+console.log(name, github);
 
-printProfileData(profileDataArgs);
+const pageHTML = generatePage(name, github);
 
-  
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
